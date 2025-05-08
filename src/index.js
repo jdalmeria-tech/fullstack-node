47156c640a6ec8fca2183ba6a58f07140bb8e7f5
jdalmeria-tech <alm24008@byui.edu>
2025-05-09 00:48:18 +0800

    anatomy of express (:

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ const app = express();
 const port = 3001; // any num between 0 - 65,535
 
 // creates get route
-app.get("/", (req, res)=>{
-  console.log("Testing");
+app.get("/:category", (req, res)=>{
+  console.log("Request URL:", req.url);
+  console.log("Request Params:", req.params);
+  console.log("Request Query:", req.query);
+
+
   res.send("Hello world!");
 });
 
