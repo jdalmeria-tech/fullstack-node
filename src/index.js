064037d9edad1da7d0db498fd0e28d66d6356a33
jdalmeria-tech <alm24008@byui.edu>
2025-05-13 12:28:18 +0800

    learning middlewares (:

--- src/index.js
+++ src/index.js
@@ -4,8 +4,15 @@ const tasksRouter = require("./tasks/tasks.router.js");
 const app = express();
 const port = 3001; // any num between 0 - 65,535
 
+const middleWare = function (req, res, next){
+  req.info = {appname: "Task Manager", author: "jdalmeria-tech"};
+  next();
+}
+
+app.use(middleWare);
+
 // define routes
-app.use("/", tasksRouter);
+app.use("/", tasksRouter); // this is a middleware
 
 // Starts the server and listens for incoming requests on the specified port
 app.listen(port, ()=>{
--- src/tasks/tasks.router.js
+++ src/tasks/tasks.router.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const tasksRouter = express.Router();
 
-tasksRouter.get("/tasks", (req, res) => res.send("All tasks"));
+tasksRouter.get("/tasks", (req, res) => {
+  console.log(req.info);
+  return res.send(req.info);
+});
 
 tasksRouter.post("/tasks", (req, res) => res.send("Create new task"));
 
