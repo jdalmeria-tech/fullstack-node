14a84533a416d8f95d1c37c9b9717cbd0c328178
jdalmeria-tech <alm24008@byui.edu>
2025-06-16 01:48:32 +0800

    expressWinston middleware (:

--- src/helpers/winston.helper.js
+++ src/helpers/winston.helper.js
@@ -8,6 +8,23 @@ const path = require("path");
   debug (4)
   silly (5)
  */
+const transports = [
+  new winston.transports.Console({
+    level: "info",
+    format: winston.format.combine(winston.format.colorize()),
+  }),
+  new winston.transports.File({
+    level: "info",
+    filename: path.join(__dirname, "..", "info.log"),
+    format: winston.format.json(),
+  }),
+  new winston.transports.File({
+    level: "error",
+    filename: path.join(__dirname, "..", "error.log"),
+    format: winston.format.json(),
+  }),
+];
+
 const logger = winston.createLogger({
   format: winston.format.combine(
     winston.format.timestamp({
@@ -17,21 +34,7 @@ const logger = winston.createLogger({
       (info) => `${info.timestamp} [${info.level}] : ${info.message}`
     )
   ),
+  transports: transports,
 });
 
-const transports = [
-  new winston.transports.Console({
-    level: "info",
-    format: winston.format.combine(winston.format.colorize()),
-  }),
-  new winston.transports.File({
-    level: "info",
-    filename: path.join(__dirname, "../..", "info.log"),
-    format: winston.format.json(),
-  }),
-  new winston.transports.File({
-    level: "error",
-    filename: path.join(__dirname, "../..", "error.log"),
-    format: winston.format.json(),
-  }),
-];
+module.exports = logger;
