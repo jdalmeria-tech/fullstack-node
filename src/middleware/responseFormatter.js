3899c3d4f34fe294d1974c7907992a0439ab3bdb
jdalmeria-tech <alm24008@byui.edu>
2025-06-24 15:58:59 +0800

    edited the final data so that data is not nested to "data" (:

--- src/middleware/responseFormatter.js
+++ src/middleware/responseFormatter.js
@@ -8,10 +8,18 @@ function responseFormatter(req, res, next) {
       status: res.statusCode >= 200 && res.statusCode < 300 ? "Success" : "Error",
       statusCode: res.statusCode,
       message: getReasonPhrase(res.statusCode),
-      data: res.statusCode >= 200 && res.statusCode < 300 ? data : null,
-      error: res.statusCode >= 200 && res.statusCode < 300 ? null : data,
-
     };
+
+    if(res.statusCode >= 200 && res.statusCode < 300) {
+      response.data = data.pagination ? data.data : data;
+    }
+    if(res.statusCode > 300){
+      response.error = data;
+    }
+
+    if(data.pagination){
+      response.pagination = data.pagination;
+    }
 
     // Call the original res.json method with the formatted response
     originalJson.call(res, response);
