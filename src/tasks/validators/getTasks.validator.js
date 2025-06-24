5e88d37addee0552acac23dcb948b5e91000213b
jdalmeria-tech <alm24008@byui.edu>
2025-06-24 15:41:06 +0800

    setting a default value (:

--- src/tasks/validators/getTasks.validator.js
+++ src/tasks/validators/getTasks.validator.js
@@ -1,11 +1,27 @@
+const { min } = require("date-fns");
 const { query } = require("express-validator");
 
 const getTasksValidator = [
-  query("limit", "Limit must be a valid integer").optional().isInt().toInt(),
-  query("page", "Page must be a valid integer").optional().isInt().toInt(),
+  query("limit", "Limit must be a valid integer")
+    .optional()
+    .isInt()
+    .toInt({ min: 1 }),
+  query("limit").customSanitizer((value, { req }) => {
+    return value ? value : 5;
+  }),
+  query("page", "Page must be a valid integer")
+    .optional()
+    .isInt()
+    .toInt({ min: 1 }),
+  query("page").customSanitizer((value, { req }) => {
+    return value ? value : 1;
+  }),
   query("order", "Order must be on of ['asc', 'dsc']")
     .optional()
     .isIn(["asc", "dsc"]),
+  query("order").customSanitizer((value, { req }) => {
+    return value ? value : "asc";
+  }),
 ];
 
 module.exports = getTasksValidator;
